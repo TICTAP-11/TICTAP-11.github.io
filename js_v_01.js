f15a5e396c6ebe6dea3315c80f139bb09edfae6d
TICTAP-11 <154484911+TICTAP-11@users.noreply.github.com>
2024-12-09 17:35:25 +0900

    2024.12.09 update all

---
+++ js_v_01.js
@@ -0,0 +1,32 @@
+
+
+/* <input type="button" value="change theme color" onclick="
+            
+        if (document.querySelector('body').style
+        .backgroundColor == 'white') {
+            document.querySelector('body').style
+            .backgroundColor = 'black'
+            document.querySelector('body').style
+            .color = 'white'
+        }
+        else {document.querySelector('body').style
+            .backgroundColor = 'white'
+            document.querySelector('body').style
+            .color = 'black'
+            };
+
+            //document.write(alert('clicked'));
+
+        "></input> */
+
+
+let theme = 0;
+
+function change_theme() {
+    if (theme = 0) {
+        alert("hahahah");
+    }
+    else {
+
+    }
+}
